2105aa8210ad7ff341dda04b2c81c0fce7487587
Craig Prevallet <penguintx@hotmail.com>
2025-12-14 16:54:46 -0600

    Add windows redistributable script.

---
+++ create_windows_dist.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Create distributable directory.  Can be anywhere - but insure consistency in subsequent commands.
+mkdir -p /c/Users/barfy/Documents/siliconsneaker2
+# Uncomment this to completely rebuild dist directory
+# cp /mingw64 /c/Users/barfy/Documents/siliconsneaker2/
+cp /home/penguin/siliconsneaker2/target/debug/siliconsneaker2.exe /c/Users/barfy/Documents/siliconsneaker2/bin/
+
